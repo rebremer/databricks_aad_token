58bb1ac6be4a7afb369b0c903053806e5a8af5a4
René Bremer <32130834+rebremer@users.noreply.github.com>
2020-09-26 16:32:55 +0200

    Update databricks_pat_token.sh

--- databricks_pat_token.sh
+++ databricks_pat_token.sh
@@ -17,7 +17,7 @@ token=$(jq .accessToken -r <<< "$token_response")
 token_response=$(az account get-access-token --resource https://management.core.windows.net/)
 azToken=$(jq .accessToken -r <<< "$token_response")
 # You can also generate a PAT token. Note the quota limit of 600 tokens.
-api_response=$(curl -v -X POST https://$loc.azuredatabricks.net"/api/2.0/token/create \
+api_response=$(curl -v -X POST https://$loc.azuredatabricks.net/api/2.0/token/create \
   -H "Authorization: Bearer $token" \
   -H "X-Databricks-Azure-SP-Management-Token:$azToken" \
   -H "X-Databricks-Azure-Workspace-Resource-Id:$wsId" \
